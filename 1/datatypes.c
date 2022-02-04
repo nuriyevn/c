0eb2c9ce027657aa836f9b21f82cff308abb19b3
nuriyevn <nuriyevn@gmail.com>
2022-02-04 14:19:19 +0200

    Create datatypes.c

---
+++ 1/datatypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int foot;
+	float mmeter = 0.0;  // +-38 zeroes,  6 digits after floating
+	int lower = 1;  // ~2 billion
+	int upper = 30;
+	int step = 1;
+	foot = lower;
+	char c = 'A'; // range is [-128..127]  -  ASCII - table
+	char c2 = 65;
+	double d = 34.56; // +- 308 zeroes, 15 digits after floating poing
+ 	unsigned char uc = 134; // Range is  [0..255]
+}
